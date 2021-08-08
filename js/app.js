c1f20764c89d3dfddc41248d4c18d5e2198a8830
Jeroen van Bart <77612186+jeroenvanbart@users.noreply.github.com>
2021-08-08 22:49:39 +0200

    started with content

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = ''
-            }else{
-                link.style.animation = `navLinkFade 2s ease forwards ${index / 7 }s`};
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 2s ease forwards ${index / 7}s`;
+      }
     });
-    burger.classList.toggle('toggle');
-    });
-}
+    burger.classList.toggle("toggle");
+  });
+};
 
 
 navSlide();
